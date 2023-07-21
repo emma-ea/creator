e9e8c26f071df1503deb8d73213dbc4ede17ac5b
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-07-21 11:33:56 +0000

    watcher wraps around widgets that need ref

---
+++ creator/lib/src/watcher.dart
@@ -0,0 +1,40 @@
+import 'package:creator/creator.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+
+class Watcher extends StatefulWidget {
+  const Watcher(this.builder, {super.key});
+
+  final Widget Function(BuildContext context, Ref ref, Creator self)? builder;
+
+  @override
+  State<Watcher> createState() => _WatcherState();
+}
+
+class _WatcherState extends State<Watcher> {
+
+  late Creator<Widget> builder;
+  late Ref ref;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    ref = CreatorGraph.of(context).ref;
+    builder = Creator((ref, self) {
+      setState(() {});
+      return widget.builder!(context, ref, self);
+    });
+  }
+
+  @override
+  void dispose() {
+    ref.dispose(builder);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ref.recreate(builder);
+    return ref.watch(builder, null);
+  }
+}
